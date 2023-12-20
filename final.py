76a7fb667fde9c2de2a4176a791780969f2f0f35
nrndita12 <154235390+nrndita12@users.noreply.github.com>
2023-12-20 16:39:38 +0700

    Update final.py

--- final.py
+++ final.py
@@ -272,7 +272,8 @@ import xml.etree.ElementTree as ET
 def fetch_bmkg_data():
     try:
         url = "https://data.bmkg.go.id/DataMKG/TEWS/gempaterkini.xml"
-        response = requests.get(url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
             data_list = []
@@ -310,5 +311,6 @@ bmkg_data = fetch_bmkg_data()
 
 if bmkg_data:
     st.table(bmkg_data)
+
 
 
